6a186d851a91f34b6d832bebe7daf2c15de3f22f
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2020-11-03 06:40:12 -0800

    Added sorting for name and date

--- MegaDeskWebApp/MegaDeskWebApp/Pages/Quotes/Index.cshtml.cs
+++ MegaDeskWebApp/MegaDeskWebApp/Pages/Quotes/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDeskWebApp.Data;
 using MegaDeskWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MegaDeskWebApp.Pages.Quotes
 {
@@ -19,11 +20,52 @@ namespace MegaDeskWebApp.Pages.Quotes
             _context = context;
         }
 
-        public IList<Quote> Quote { get;set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Book { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CustomerName { get; set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public IList<Quote> Quote { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
+            // using System 
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            NameSort = sortOrder == "Name" ? "name_desc" : "Name";
+
+            IQueryable<Quote> quoteQuery = from s in _context.Quote
+                                                   select s;
+
+            var quotes = from m in _context.Quote
+                             select m;
+
+            //switch case for sorting
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    quotes = quotes.OrderBy(s => s.CustomerName);
+                    break;
+                case "Name":
+                    quotes = quotes.OrderByDescending(s => s.CustomerName);
+                    break;
+                case "Date":
+                    quotes = quotes.OrderBy(s => Convert.ToDateTime(s.QuoteDate));
+                    break;
+                case "date_desc":
+                    quotes = quotes.OrderByDescending(s => Convert.ToDateTime(s.QuoteDate));
+                    break;
+                default:
+                    quotes = quotes.OrderBy(s => s.ID);
+                    break;
+            }
+
             Quote = await _context.Quote.ToListAsync();
+            Quote = await quotes.ToListAsync();
+
         }
     }
 }
