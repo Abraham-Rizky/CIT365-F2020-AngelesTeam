c9f50a04c2bdc46df52fa56e618ce962e7790506
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2020-11-03 05:59:21 -0800

    Fixed Change page.

--- MegaDeskWebApp/MegaDeskWebApp/Pages/Quotes/Create.cshtml.cs
+++ MegaDeskWebApp/MegaDeskWebApp/Pages/Quotes/Create.cshtml.cs
@@ -58,25 +58,32 @@ namespace MegaDeskWebApp.Pages.Quotes
             }
 
             decimal materialCost;
+            string myMaterial;
             switch (Int32.Parse (materialIndex) )
             {
                 case 1:
                     materialCost = 200;
+                    myMaterial = "Oak";
                     break;
                 case 2:
                     materialCost = 100;
+                    myMaterial = "Laminate";
                     break;
                 case 3:
                     materialCost = 50;
+                    myMaterial = "Pine";
                     break;
                 case 4:
                     materialCost = 300;
+                    myMaterial = "Rosewood";
                     break;
                 case 5:
                     materialCost = 125;
+                    myMaterial = "Veneer";
                     break;
                 default:
                     materialCost = 0;
+                    myMaterial = "";
                     break;
             }
 
@@ -91,36 +98,60 @@ namespace MegaDeskWebApp.Pages.Quotes
             }
 
             int shippingAreaIndex;
-            switch (shippingIndex)
+            if(area < 1000)
             {
-                case 0:
-                    shippingAreaIndex = 0;
-                    break;
-                case 1:
-                    shippingAreaIndex = 1;
-                    break;
-                case 2:
-                    shippingAreaIndex = 2;
-                    break;
-                case 3:
-                    shippingAreaIndex = 3;
-                    break;
-                default:
-                    shippingAreaIndex = 0;
-                    break;
+                shippingAreaIndex = 0;
+            } else if (area > 1000 && area < 2000){
+                shippingAreaIndex = 1;
+            } else if (area > 2000)
+            {
+                shippingAreaIndex = 2;
+            }
+            else
+            {
+                shippingAreaIndex = 0;
             }
 
 
-            var shippingCost = shippingArray[shippingIndex, shippingAreaIndex];
+            var shippingCost = 0;
+            var myShipping = "";
+            if(shippingIndex == 99)
+            {
+                shippingCost = 0;
+                myShipping = "Standard 14 days";
+            }
+            else
+            {
+                shippingCost = shippingArray[shippingIndex, shippingAreaIndex];
+                switch (shippingIndex)
+                {
+                    case 0:
+                        myShipping = "3 Days";
+                        break;
+                    case 1:
+                        myShipping = "5 Days";
+                        break;
+                    case 2:
+                        myShipping = "7 Days";
+                        break;
+                    default:
+                        myShipping = "";
+                        break;
+                }
+            }
+
+            // Calculdate drawer cost at $50 each
             var drawerCost = Int32.Parse(drawerCount) * 50;
+
+            // POST all required fields
             Quote.CustomerName = name;
             Quote.Width = Int32.Parse(width);
             Quote.Depth = Int32.Parse(depth);
             Quote.Area = area;
             Quote.DrawerCount = Int32.Parse(drawerCount);
             Quote.DrawerCost = drawerCost;
-            Quote.DeskMaterial = material;
-            Quote.ShippingOption = Request.Form["shipping"];
+            Quote.DeskMaterial = myMaterial;
+            Quote.ShippingOption = myShipping;
             Quote.QuoteDate = todayDate;
             Quote.OversizeCost = oversizeCost;
             Quote.MaterialCost = materialCost;
