3d1d636065d450d67ff1ff97796a3714ee09133b
Landon Hymas <landon.hymas13@gmail.com>
2020-11-04 02:16:41 -0700

    Partialy done with tool tips

--- MegaDeskWebApp/MegaDeskWebApp/wwwroot/js/site.js
+++ MegaDeskWebApp/MegaDeskWebApp/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// Code for tool tip with images
+var Path = '/images/';
+var ImgAry = new Array('', 'oak_3.png', 'laminate_2.png', 'pine_2.png', 'rosewood_2.png', 'veneer_2.png');
+
+function Swap(obj, id) {
+    var i = obj.selectedIndex;
+    if (i < 1) { return; }
+    document.getElementById(id).src = Path + ImgAry[i];
+}
