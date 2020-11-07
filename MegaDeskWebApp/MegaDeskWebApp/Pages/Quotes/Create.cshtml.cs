9cebdc286dd3000f26cfc565019f4f2ec01136f1
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2020-11-06 17:20:52 -0800

    Edit create.cshtml

--- MegaDeskWebApp/MegaDeskWebApp/Pages/Quotes/Create.cshtml.cs
+++ MegaDeskWebApp/MegaDeskWebApp/Pages/Quotes/Create.cshtml.cs
@@ -33,133 +33,139 @@ namespace MegaDeskWebApp.Pages.Quotes
             {
                 return Page();
             }
-            Quote Quote = new Quote();
-            string name = Request.Form["name"];
-            string width = Request.Form["width"];
-            string depth = Request.Form["depth"];
-            string drawerCount = Request.Form["drawercount"];
-            string material = Request.Form["material"];
-            int shippingIndex = Convert.ToInt32 (Request.Form["shipping"]);
-            string materialIndex = Request.Form["material"];
-            DateTime todayDate = DateTime.Now;
-            
-            // Calculate area
-            var area = Int32.Parse( width) * Int32.Parse(depth);
-            
-            // Calculate oversize cost
-            decimal oversizeCost;
-            if(area > 1000)
-            {
-                oversizeCost = area - 1000;
-            }
-            else
-            {
-                oversizeCost = 0;
-            }
 
-            decimal materialCost;
-            string myMaterial;
-            switch (Int32.Parse (materialIndex) )
-            {
-                case 1:
-                    materialCost = 200;
-                    myMaterial = "Oak";
-                    break;
-                case 2:
-                    materialCost = 100;
-                    myMaterial = "Laminate";
-                    break;
-                case 3:
-                    materialCost = 50;
-                    myMaterial = "Pine";
-                    break;
-                case 4:
-                    materialCost = 300;
-                    myMaterial = "Rosewood";
-                    break;
-                case 5:
-                    materialCost = 125;
-                    myMaterial = "Veneer";
-                    break;
-                default:
-                    materialCost = 0;
-                    myMaterial = "";
-                    break;
-            }
+                Quote Quote = new Quote();
+                string name = Request.Form["name"];
+                string width = Request.Form["width"];
+                string depth = Request.Form["depth"];
+                string drawerCount = Request.Form["drawercount"];
+                string material = Request.Form["material"];
+                int shippingIndex = Convert.ToInt32(Request.Form["shipping"]);
+                string materialIndex = Request.Form["material"];
+                DateTime todayDate = DateTime.Now;
 
-            // Calculate shipping
-            int[] shipping = new int[] { 60, 70, 80, 40, 50, 60, 30, 35, 40 };
+                // Calculate area
+                var area = Int32.Parse(width) * Int32.Parse(depth);
 
-            int[,] shippingArray = new int[3, 3];
-
-            for (int i = 0; i < shipping.Length; i++)
-            {
-                shippingArray[i / 3, i % 3] = shipping[i];
-            }
-
-            int shippingAreaIndex;
-            if(area < 1000)
-            {
-                shippingAreaIndex = 0;
-            } else if (area > 1000 && area < 2000){
-                shippingAreaIndex = 1;
-            } else if (area > 2000)
-            {
-                shippingAreaIndex = 2;
-            }
-            else
-            {
-                shippingAreaIndex = 0;
-            }
-
-
-            var shippingCost = 0;
-            var myShipping = "";
-            if(shippingIndex == 99)
-            {
-                shippingCost = 0;
-                myShipping = "Standard 14 Days";
-            }
-            else
-            {
-                shippingCost = shippingArray[shippingIndex, shippingAreaIndex];
-                switch (shippingIndex)
+                // Calculate oversize cost
+                decimal oversizeCost;
+                if (area > 1000)
                 {
-                    case 0:
-                        myShipping = "3 Days";
-                        break;
+                    oversizeCost = area - 1000;
+                }
+                else
+                {
+                    oversizeCost = 0;
+                }
+
+                decimal materialCost;
+                string myMaterial;
+                switch (Int32.Parse(materialIndex))
+                {
                     case 1:
-                        myShipping = "5 Days";
+                        materialCost = 200;
+                        myMaterial = "Oak";
                         break;
                     case 2:
-                        myShipping = "7 Days";
+                        materialCost = 100;
+                        myMaterial = "Laminate";
+                        break;
+                    case 3:
+                        materialCost = 50;
+                        myMaterial = "Pine";
+                        break;
+                    case 4:
+                        materialCost = 300;
+                        myMaterial = "Rosewood";
+                        break;
+                    case 5:
+                        materialCost = 125;
+                        myMaterial = "Veneer";
                         break;
                     default:
-                        myShipping = "";
+                        materialCost = 0;
+                        myMaterial = "";
                         break;
                 }
-            }
 
-            // Calculdate drawer cost at $50 each
-            var drawerCost = Int32.Parse(drawerCount) * 50;
+                // Calculate shipping
+                int[] shipping = new int[] { 60, 70, 80, 40, 50, 60, 30, 35, 40 };
 
-            // POST all required fields
-            Quote.CustomerName = name;
-            Quote.Width = Int32.Parse(width);
-            Quote.Depth = Int32.Parse(depth);
-            Quote.Area = area;
-            Quote.DrawerCount = Int32.Parse(drawerCount);
-            Quote.DrawerCost = drawerCost;
-            Quote.DeskMaterial = myMaterial;
-            Quote.ShippingOption = myShipping;
-            Quote.QuoteDate = todayDate;
-            Quote.OversizeCost = oversizeCost;
-            Quote.MaterialCost = materialCost;
-            Quote.ShippingCost = shippingCost;
+                int[,] shippingArray = new int[3, 3];
 
-            // Calculate total cost
-            decimal totalCost = 200 + oversizeCost + drawerCost + materialCost + shippingCost;
-            Quote.TotalCost = totalCost;
+                for (int i = 0; i < shipping.Length; i++)
+                {
+                    shippingArray[i / 3, i % 3] = shipping[i];
+                }
+
+                int shippingAreaIndex;
+                if (area < 1000)
+                {
+                    shippingAreaIndex = 0;
+                }
+                else if (area > 1000 && area < 2000)
+                {
+                    shippingAreaIndex = 1;
+                }
+                else if (area > 2000)
+                {
+                    shippingAreaIndex = 2;
+                }
+                else
+                {
+                    shippingAreaIndex = 0;
+                }
+
+
+                var shippingCost = 0;
+                var myShipping = "";
+                if (shippingIndex == 99)
+                {
+                    shippingCost = 0;
+                    myShipping = "Standard 14 Days";
+                }
+                else
+                {
+                    shippingCost = shippingArray[shippingIndex, shippingAreaIndex];
+                    switch (shippingIndex)
+                    {
+                        case 0:
+                            myShipping = "3 Days";
+                            break;
+                        case 1:
+                            myShipping = "5 Days";
+                            break;
+                        case 2:
+                            myShipping = "7 Days";
+                            break;
+                        default:
+                            myShipping = "";
+                            break;
+                    }
+                }
+
+                // Calculdate drawer cost at $50 each
+                var drawerCost = Int32.Parse(drawerCount) * 50;
+
+                // POST all required fields
+                Quote.CustomerName = name;
+                Quote.Width = Int32.Parse(width);
+                Quote.Depth = Int32.Parse(depth);
+                Quote.Area = area;
+                Quote.DrawerCount = Int32.Parse(drawerCount);
+                Quote.DrawerCost = drawerCost;
+                Quote.DeskMaterial = myMaterial;
+                Quote.ShippingOption = myShipping;
+                Quote.QuoteDate = todayDate;
+                Quote.OversizeCost = oversizeCost;
+                Quote.MaterialCost = materialCost;
+                Quote.ShippingCost = shippingCost;
+
+                // Calculate total cost
+                decimal totalCost = 200 + oversizeCost + drawerCost + materialCost + shippingCost;
+                Quote.TotalCost = totalCost;
+            
+
 
 
             _context.Quote.Add(Quote);
