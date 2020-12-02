c8d298de623c49f02d7ab1c277fa1d97e93769b6
Abraham-Rizky <rizky@DESKTOP-5ORJKVE>
2020-12-01 22:44:20 -0800

    Created 2 models

---
+++ SacramentMeetingPlanner/Models/Meetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Meetings
+    {
+        public int ID { get; set; }
+        public DateTime MeetingDate { get; set; }
+        public string Conductor { get; set; }
+        public string OpeningHymn { get; set; }
+        public string Invocation { get; set; }
+        public string SacramentHymn { get; set; }
+        public ICollection<Speaker> Speakers { get; set; }
+        public string ClosingHymn { get; set; }
+        public string Benediction { get; set; }
+    }
+}
---
+++ SacramentMeetingPlanner/Models/Speaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Speaker
+    {
+        public int SpeakerID { get; set; }
+        public DateTime MeetingDate { get; set; }
+        public int SpeakerIndex { get; set; }
+        public string FullName { get; set; }
+        public string Topic { get; set; }
+    }
+}
